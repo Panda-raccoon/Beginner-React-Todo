6b65f723a3971f7746522a38df43a630315d3327
Panda-racoon <millie3519@gmail.com>
2024-08-19 10:05:45 +0900

    vercel추가

---
+++ api/hello.ts
@@ -0,0 +1,6 @@
+// import axios from 'axios'
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+  res.status(200).json('Hello World')
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-// import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx' 
-import { router } from './routes' //index.tsx 는 기본값이어서 생략이 가능 원래는 -> './routes/index.tsx'
 import { RouterProvider } from 'react-router-dom'
+import { router } from './routes' //index.tsx 는 기본값이어서 생략이 가능 원래는 -> './routes/index.tsx'
+// import App from './App.tsx' 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 <RouterProvider router={router} />
 )
+
+;(async () => {
+  const res = await fetch('/api/hello', {
+    method: 'POST'
+  })
+  const data = await res.json()
+  console.log('/api/hello :::', data)
+})()
