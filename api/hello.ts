8402cdcad2d5abfd3e65bba15f4fca5a2d30ef79
Panda-racoon <millie3519@gmail.com>
2024-08-19 10:43:17 +0900

    api 내용 수정

--- api/hello.ts
+++ api/hello.ts
@@ -2,5 +2,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function (req: VercelRequest, res: VercelResponse) {
-  res.status(200).json('Hello World')
+  res.status(200).json({
+    name: 'panda',
+    age: 85
+  })
 }
